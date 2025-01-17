528d18b773259dd1fb9efc5f5bba92d5db93c6d1
rowan oliver weaver <159619990+RooOliver-Weaver@users.noreply.github.com>
2025-01-17 09:57:14 +0000

    Almost there just got to add to faves

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -2,4 +2,24 @@ class FavouritesController < ApplicationController
   def index
     @favourites = Favourite.all
   end
+
+  def create
+    @car = Car.find(params[:car_id])
+    @favourite = Favourite.new(car: @car)
+    if @favourite.save
+      redirect_to car_path(@car), notice: "Saved to your favourites"
+    end
+  end
+
+  def destroy
+    @favourite = Favourite.find(params[:id])
+    @favourite.destroy
+    redirect_to favourites_path, notice: "Favourite was deleted"
+  end
+
+  private
+
+  def favourite_params
+    params.require(:favourite).permit(:car_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :cars, only: [ :index, :show ] do
     resources :reviews, only: [ :new, :create ]
   end
-  resources :favourites, only: [ :index ]
+  resources :favourites, only: [ :index, :create, :destroy ]
   root "cars#index"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
